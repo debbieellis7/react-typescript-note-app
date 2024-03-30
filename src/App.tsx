9ca80d9a8fbd8048f791e9da3ea810615d474f4c
debbieellis7 <1debbieellis@gmail.com>
2024-03-30 18:18:42 +0800

    Add NoteList component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useMemo } from 'react'
 import { Container } from 'react-bootstrap'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid'
 import { NewNote } from './NewNote'
 import { useLocalStorage } from './useLocalStorage'
-import { v4 as uuidV4 } from 'uuid'
+import { NoteList } from './NoteList'
 
 export type Note = {
     id: string
@@ -57,7 +58,7 @@ function App() {
     return (
         <Container className='my-4'>
             <Routes>
-                <Route path='/' element={<h1>Home</h1>} />
+                <Route path='/' element={<NoteList notes={notesWithTags} availableTags={tags} />} />
                 <Route path='/new' element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
                 <Route path='/:id'>
                     <Route index element={<h1>Show</h1>} />
